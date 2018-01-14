b3107a3f1144f6dca107d20ce1c1ba0e93605f0f
ikejikeji <jun.ikejima@gmail.com>
2018-01-14 07:31:41 +0000

    プッシュし直し

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,24 @@
+class StaticPagesController < ApplicationController
+  def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      if params[:q]
+        relation = Micropost.joins(:user)
+        @feed_items = relation.merge(User.search_by_keyword(params[:q]))
+                        .or(relation.search_by_keyword(params[:q]))
+                        .paginate(page: params[:page])
+      else
+        @feed_items = current_user.feed.paginate(page: params[:page])
+      end
+    end
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
