d30631d777188499d1cfadffc98fb13c62225424
ikejikeji <jun.ikejima@gmail.com>
2018-01-24 11:26:57 +0000

    ログイン方法改修

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save # => Validation
       # Sucess
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+#      @user.send_activation_email
+#      flash[:info] = "Please check your email to activate your account."
+      redirect_to @user
     else
       # Failure
       render 'new'      
